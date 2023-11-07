38859dbc4467c1ebd229ea1bcaff76f2b0bfcf07
Aisyah Sulaiman <aisyahsulaiman98@gmail.com>
2023-11-07 15:24:57 +0800

    next-job

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,2 @@
 // place files you want to import through the `$lib` alias in this folder.
+<span class="text-sm">USD {humanize.formatNumber(job.minAnnualCompensation)} - USD {humanize.formatNumber(job.maxAnnualCompensation)}</span>
